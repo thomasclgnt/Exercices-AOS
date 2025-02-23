d719878f1298d5f702a501cd7f377bbcf75f1fdc
Thomas Cayla Ginestet <caylagin@insa-toulouse.fr>
2025-02-23 21:02:39 +0100

    microservice ex partie 3

--- MicroService/StudentEvalService/StudentEvalService/src/main/java/fr/insa/mas/StudentEvalService/StudentEvalServiceApplication.java
+++ MicroService/StudentEvalService/StudentEvalService/src/main/java/fr/insa/mas/StudentEvalService/StudentEvalServiceApplication.java
@@ -2,6 +2,7 @@ package fr.insa.mas.StudentEvalService;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 public class StudentEvalServiceApplication {
--- MicroService/StudentsListService/StudentsListService/src/main/java/fr/insa/mas/StudentsListService/resources/StudentListResource.java
+++ MicroService/StudentsListService/StudentsListService/src/main/java/fr/insa/mas/StudentsListService/resources/StudentListResource.java
@@ -3,6 +3,7 @@ package fr.insa.mas.StudentsListService.resources;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,6 +18,9 @@ import fr.insa.mas.StudentsListService.model.StudentInfos;
 @RestController
 @RequestMapping("/students")
 public class StudentListResource {
+	
+	@Autowired
+	private RestTemplate restTemplate;
 	
 	@GetMapping("ids/{idSpeciality}")
 	public StudentIDList getIDStudents(@PathVariable("idSpeciality") String speciality) {
@@ -41,17 +45,23 @@ public class StudentListResource {
 		students.addStudentToList(3);
 		
 		//Instanciate RestTemplate for Rest calls
-		RestTemplate restTemplate = new RestTemplate();
+//		RestTemplate restTemplate = new RestTemplate(); 
+		// On supprime cette instanciation qui n'est plus nécessaire dans la partie 3 avec Bean et l'autowired.
 		int i = 0;
 		List<Student> listStudents = new ArrayList<Student>();
 		
 		while (i < students.getStudentList().size()) {
 			//Call the MS StudentInfoService to get student's information.
-			StudentInfos etudInfos = restTemplate.getForObject("http://localhost:8081/student/"+ i, StudentInfos.class);
+//			StudentInfos etudInfos = restTemplate.getForObject("http://localhost:8081/student/"+ i, StudentInfos.class);
+			// On modifie l'appel avec l'url directe du ms tel que référencé par Eureka 
+			StudentInfos etudInfos = restTemplate.getForObject("http://StudentInfoService/student/"+ i, StudentInfos.class);
 			
 			//Call the MS StudentEvalService to get student's evaluation
 			//The result is deserialized into Evaluation java object
-			Evaluation eval = restTemplate.getForObject("http://localhost:8082/evaluation/"+ i, Evaluation.class);
+//			Evaluation eval = restTemplate.getForObject("http://localhost:8082/evaluation/"+ i, Evaluation.class);
+			// On modifie l'appel avec l'url directe du ms tel que référencé par Eureka 
+			Evaluation eval = restTemplate.getForObject("http://StudentEvalService/evaluation/"+ i, Evaluation.class);
+
 			
 			//Instanciate a student with his id, firt and last Name, average and store it in the list
 			listStudents.add(new Student(i, etudInfos.getFirstName(), etudInfos.getLastName(), eval.getAverage()));
