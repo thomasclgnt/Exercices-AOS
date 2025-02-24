90b8ebdc376e4c48239fc252dbf4d26372eda0be
Thomas Cayla Ginestet <caylagin@insa-toulouse.fr>
2025-02-24 17:15:24 +0100

    microservice exo partie 6 OK

--- MicroService/StudentInfoService/StudentInfoService/src/main/java/fr/insa/mas/StudentInfoService/resources/StudentInfoResource.java
+++ MicroService/StudentInfoService/StudentInfoService/src/main/java/fr/insa/mas/StudentInfoService/resources/StudentInfoResource.java
@@ -3,6 +3,8 @@ package fr.insa.mas.StudentInfoService.resources;
 import java.util.Arrays;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,6 +16,55 @@ import fr.insa.mas.StudentInfoService.model.StudentInfos;
 @RequestMapping("/student")
 public class StudentInfoResource {
 	
+	@Value("${db.uri}")
+	private String dbUri;
+	
+	@Value("${db.name}")
+	private String dbName;
+	
+	@Value("${db.login}")
+	private String dbLogin;
+	
+	@Value("${db.pwd}")
+	private String dbPwd;
+
+	@GetMapping("/dbUri")
+	public String getDbUri() {
+		return dbUri;
+	}
+
+	public void setDbUri(String dbUri) {
+		this.dbUri = dbUri;
+	}
+
+	@GetMapping("/dbName")
+	public String getDbName() {
+		return dbName;
+	}
+
+	public void setDbName(String dbName) {
+		this.dbName = dbName;
+	}
+
+	@GetMapping("/dbLog")
+	public String getDbLogin() {
+		return dbLogin;
+	}
+
+	public void setDbLogin(String dbLogin) {
+		this.dbLogin = dbLogin;
+	}
+
+	@GetMapping("/dbPwd")
+	public String getDbPwd() {
+		return dbPwd;
+	}
+
+	public void setDbPwd(String dbPwd) {
+		this.dbPwd = dbPwd;
+	}
+	
+	
 	@GetMapping("/{idStudent}")
 	public StudentInfos getInfoEtudiant(@PathVariable("idStudent") int id) {
 		
@@ -24,7 +75,10 @@ public class StudentInfoResource {
 				new StudentInfos(2, "Azi", "Sana", "22/05/1992"),
 				new StudentInfos(3, "Yala", "Nelia", "12/06/1994")
 				);
-		System.out.println("Called");
+		
+		System.out.println("Called...");
+		System.out.println("Connection à la BDD : " + this.getDbName() + " à l'URI : " + this.getDbUri());
+		
 		//Get the student that corresponds to the id
 		return etudInfos.get(id);
 	}
